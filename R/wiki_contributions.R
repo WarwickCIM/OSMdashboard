0d9050aabd3a346f18e7628d2c0a6dd3b32efde2
Carlos Cámara <carlos.camara@warwick.ac.uk>
2025-04-11 12:46:15 +0100

    Task #16 - Delete duplicated function
    stats_contributions_wiki() was doing the same as calc_stats_contributions_wiki(), which is probably a better name

--- R/wiki_contributions.R
+++ R/wiki_contributions.R
@@ -37,53 +37,6 @@ get_contributions_wiki <- function(users) {
 }
 
 
-#' Wiki contributions summaries
-#'
-#' @description
-#' Provides a summary of contributions
-#'
-#' @param df a dataframe with all wiki contributions generated from [get_contributions_wiki()]
-#'
-#' @returns a list containing several summarised data.
-#' @export
-#'
-stats_contributions_wiki <- function(df) {
-  n_users <- nlevels(as.factor(df$user))
-
-  date_start <- min(df$timestamp)
-  date_end <- max(df$timestamp)
-
-  contributions_n <- df |>
-    dplyr::count(user)
-
-  contributions_size <- df |>
-    dplyr::select(user, sizediff) |>
-    dplyr::mutate(
-      additions = dplyr::case_when(sizediff > 0 ~ sizediff),
-      deletions = dplyr::case_when(sizediff < 0 ~ sizediff)
-    ) |>
-    dplyr::group_by(user) |>
-    dplyr::summarise(dplyr::across(c(additions, deletions), mean, na.rm = TRUE))
-
-
-  contributed_pages <- df |>
-    dplyr::select(title, sizediff) |>
-    dplyr::mutate(
-      sizediff = abs(sizediff),
-      # Redact usernames
-      title = dplyr::case_when(
-        stringr::str_starts(title, "User:") ~ "User pages",
-        .default = title
-      )
-    ) |>
-    dplyr::count(title, wt = sizediff, sort = TRUE)
-
-  results <- list(n_users, date_start, date_end, contributions_size, contributed_pages)
-
-  return(results)
-}
-
-
 #' Generates a summary of wiki contributions
 #'
 #' @param df a dataframe with all wiki contributions generated from get_wiki_contributions()
